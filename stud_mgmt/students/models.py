81361f6f56230755ba820ba8c6f9fe96becd5ba8
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-27 13:13:46 +0530

    feat: add Student model with user and course relationships

--- stud_mgmt/students/models.py
+++ stud_mgmt/students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from courses.models import Course
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrolled_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
