8a37b1da0abf6bfed415610a4760e02a18e20687
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-27 14:57:51 +0530

    feat: add login functionality with redirect to dashboard and update URL patterns

--- stud_mgmt/stud_mgmt/settings.py
+++ stud_mgmt/stud_mgmt/settings.py
@@ -124,5 +124,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/accounts/dashboard/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+
--- stud_mgmt/stud_mgmt/urls.py
+++ stud_mgmt/stud_mgmt/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect("accounts:dashboard")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
+    path("", home, name="home"),
+    path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
     path('students/', include('students.urls')),
 ]
