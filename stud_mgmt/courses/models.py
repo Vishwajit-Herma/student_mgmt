ee7b6a1fcf17c32f12904cdc1399680152d0786a
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-27 13:12:38 +0530

    feat: add Course model with teacher foreign key and string representation

--- stud_mgmt/courses/models.py
+++ stud_mgmt/courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={"profile__role": "TEACHER"}
+    )
+
+    def __str__(self):
+        return self.name
